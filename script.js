5807acc9128e6467ab79e9a0b75a06b809338853
singarapuMounika <166783770+singarapuMounika@users.noreply.github.com>
2024-06-29 23:01:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+let search = document.querySelector('.searchbar');
+
+document.querySelector('#search').onclick = () => {
+    search.classList.toggle('active');
+    favourite.classList.remove('active');
+    checkout.classList.remove('active');
+    login.classList.remove('active');
+}
+
+let favourite = document.querySelector('.favourite');
+
+document.querySelector('#favourite').onclick = () => {
+    favourite.classList.toggle('active');
+    search.classList.remove('active');
+    checkout.classList.remove('active');
+    login.classList.remove('active');
+}
+
+
+let checkout = document.querySelector('.checkout');
+
+document.querySelector('#checkout').onclick = () => {
+    checkout.classList.toggle('active');
+    search.classList.remove('active');
+    favourite.classList.remove('active');
+    login.classList.remove('active');
+
+}
+
+
+let login = document.querySelector('.login');
+
+document.querySelector('#login').onclick = () => {
+    login.classList.toggle('active');
+    search.classList.remove('active');
+    favourite.classList.remove('active');
+    checkout.classList.remove('active');
+}
+Window.onscroll=() =>{
+    search.classList.remove('active');
+    favourite.classList.remove('active');
+    checkout.classList.remove('active');
+    login.classList.remove('active');
+}
